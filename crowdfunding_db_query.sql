c5ca97093dde69812d31a93a367e2b977bf01ca7
Lex1996 <rockonmc04@gmail.com>
2023-07-24 19:23:06 -0400

    Final changes

---
+++ crowdfunding_db_query.sql
@@ -0,0 +1,11 @@
+SELECT * FROM public.campaign
+ORDER BY cf_id ASC LIMIT 100
+
+SELECT * FROM public.category
+ORDER BY category_id ASC LIMIT 100
+
+SELECT * FROM public.contacts
+ORDER BY contact_id ASC LIMIT 100
+
+SELECT * FROM public.subcategory
+ORDER BY subcategory_id ASC LIMIT 100
